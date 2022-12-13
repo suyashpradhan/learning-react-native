6a8df06ab508c6e9d381af5e3cbcd668545682b3
Suyash Pradhan <pradhansuyash39@gmail.com>
2022-12-13 22:39:11 +0530

    fetching data

--- App.tsx
+++ App.tsx
@@ -1,20 +1,50 @@
+/** @format */
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { ActivityIndicator, Button, FlatList, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [data, setData] = useState([]);
+	const [isLoading, setIsLoading] = useState('');
+
+	const fetchData = () => {
+		return fetch('https://reactnative.dev/movies.json')
+			.then((response) => response.json())
+			.then((json) => setData(json.movies))
+			.catch((e) => console.log(e));
+	};
+
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	return (
+		<View style={styles.container}>
+			<StatusBar style='auto' />
+			<Text>Fetching Data</Text>
+			{isLoading ? (
+				<ActivityIndicator />
+			) : (
+				<FlatList
+					data={data}
+					keyExtractor={({ id }, index) => id}
+					renderItem={({ item }) => (
+						<Text>
+							{item.title}, {item.releaseYear}
+						</Text>
+					)}
+				/>
+			)}
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
---
+++ README.md
@@ -0,0 +1,3 @@
+<!-- @format -->
+
+# Learning React Native
